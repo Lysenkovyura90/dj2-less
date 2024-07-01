a8183bc84911712621c959346fdf0045d4e5bd97
Lysenkov Yurii <lysenkovyura90@gmail.com>
2024-07-01 14:29:28 +0500

    first commit

---
+++ calculator/views.py
@@ -0,0 +1,98 @@
+from typing import Dict
+
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+
+def write_omlet(request):
+
+    context = {
+      'recipe': {
+
+      }
+    }
+
+    ingradients = DATA['omlet']
+
+    context["recipe"] = ingradients
+
+    servings_number = int(request.GET.get('servings', 1))
+    ing_count = {}
+    for ingr, amount in ingradients.items():
+        ing_count[ingr] = amount*servings_number
+
+    CONTEXT = [ing_count]
+    paginator = Paginator(CONTEXT, 10)
+    servings = paginator.get_page(servings_number)
+    context["servings"] = servings
+    return render(request, 'calculator/index.html', context)
+
+def write_pasta(request):
+    context = {
+        'recipe': {
+
+        }
+    }
+    ingradients = DATA['pasta']
+    context["recipe"] = ingradients
+    servings_number = int(request.GET.get('servings', 1))
+    ing_count = {}
+    for ingr, amount in ingradients.items():
+        ing_count[ingr] = amount * servings_number
+
+    CONTEXT = [ing_count]
+    paginator = Paginator(CONTEXT, 10)
+    servings = paginator.get_page(servings_number)
+    context["servings"] = servings
+    return render(request, 'calculator/index.html', context)
+
+
+def write_buter(request):
+    context = {
+        'recipe': {
+
+        }
+    }
+    ingradients = DATA['buter']
+    context["recipe"] = ingradients
+    servings_number = int(request.GET.get('servings', 1))
+    ing_count = {}
+    for ingr, amount in ingradients.items():
+        ing_count[ingr] = amount * servings_number
+
+    CONTEXT = [ing_count]
+    paginator = Paginator(CONTEXT, 10)
+    servings = paginator.get_page(servings_number)
+    context["servings"] = servings
+
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
